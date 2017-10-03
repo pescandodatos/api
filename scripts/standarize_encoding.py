6e2f1e038b2acad5224c48608e8c6152b0d60036
gaba <gaba@riseup.net>
2017-10-03 08:09:45 -0700

    Mas comentarios.

--- scripts/standarize_encoding.py
+++ scripts/standarize_encoding.py
@@ -30,17 +30,17 @@ def main():
         'permisos',
         'solicitudes_may',
         'unidades'
-    ]                 
+    ]
 
-    conn = sqlite3.connect('app/datos/pescando.db')
+    conn = sqlite3.connect('app/datos/pescando2.db')
     c = conn.cursor()
     u = conn.cursor()
 
-    
+
     for table in tables:
         print table
-        
-        print "NOMBRE\n"        
+
+        print "NOMBRE\n"
         # STANDARIZE NOMBRE
         select_query = """SELECT nombre FROM {table}"""
         update_query = """UPDATE {table} SET nombre = '{nuevo_estado}'"""
@@ -53,7 +53,7 @@ def main():
                     u.execute(update_query.format(table=table, nuevo_estado=nuevo_estado))
                 except:
                     print "--------- Failed on %s" % estado
-                    print sys.exc_info()[1]        
+                    print sys.exc_info()[1]
         except:
             print "> Failed selecting all from %s" % table
             print sys.exc_info()[1]
@@ -71,7 +71,7 @@ def main():
                     u.execute(update_query.format(table=table, nuevo_beneficiario=nuevo_beneficiario))
                 except:
                     print "--------- Failed on %s" % beneficiario
-                    print sys.exc_info()[1]        
+                    print sys.exc_info()[1]
         except:
             print "> Failed selecting all from %s" % table
             print sys.exc_info()[1]
@@ -89,7 +89,7 @@ def main():
                     u.execute(update_query.format(table=table, nuevo_municipio=nuevo_municipio))
                 except:
                     print "--------- Failed on %s" % municipio
-                    print sys.exc_info()[1]        
+                    print sys.exc_info()[1]
         except:
             print "> Failed selecting all from %s" % table
             print sys.exc_info()[1]
@@ -107,7 +107,7 @@ def main():
                     u.execute(update_query.format(table=table, nuevo_localidad=nuevo_localidad))
                 except:
                     print "--------- Failed on %s" % localidad
-                    print sys.exc_info()[1]        
+                    print sys.exc_info()[1]
         except:
             print "> Failed selecting all from %s" % table
             print sys.exc_info()[1]
@@ -125,7 +125,7 @@ def main():
                     u.execute(update_query.format(table=table, nuevo_estado=nuevo_estado))
                 except:
                     print "--------- Failed on %s" % estado
-                    print sys.exc_info()[1]        
+                    print sys.exc_info()[1]
         except:
             print "> Failed selecting all from %s" % table
             print sys.exc_info()[1]
