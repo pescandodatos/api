ef67cd12971223d27faa1f17bdcc54c8a81997f7
gaba <gaba@riseup.net>
2017-10-11 11:23:50 -0700

    Le cambio el nombre al log

--- app/__init__.py
+++ app/__init__.py
@@ -10,8 +10,7 @@ db = SQLAlchemy(app)
 if not app.debug and os.environ.get('HEROKU') is None:
     import logging
     from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('tmp/microblog.log', 'a',
-                                       1 * 1024 * 1024, 10)
+    file_handler = RotatingFileHandler('tmp/api.log', 'a', 1 * 1024 * 1024, 10)
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(logging.Formatter(
         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
